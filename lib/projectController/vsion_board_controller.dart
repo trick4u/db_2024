abe6d814233c43b9845b0da26094bac35c38f656
tushar-ventura <thinkbrain10@gmail.com>
2024-09-24 15:15:44 +0530

    works

--- lib/projectController/vsion_board_controller.dart
+++ lib/projectController/vsion_board_controller.dart
@@ -270,7 +270,7 @@ class VisionBoardController extends GetxController {
   DateTime _getNextAvailableTime(bool isMorning) {
     DateTime now = DateTime.now();
     DateTime baseTime = isMorning
-        ? DateTime(now.year, now.month, now.day, 14, 40)
+        ? DateTime(now.year, now.month, now.day, 08, 00)
         : DateTime(now.year, now.month, now.day, 22, 0);
 
     if (baseTime.isBefore(now)) {
